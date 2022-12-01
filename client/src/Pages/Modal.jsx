df99265df79cec7aa42c3696a0bb4a4af7e4b9f6
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2022-12-01 17:21:00 -0600

    Removed one nav bar button

--- client/src/Pages/Modal.jsx
+++ client/src/Pages/Modal.jsx
@@ -18,32 +18,34 @@ const Modal = ({ setIsOpen }) => {
 
   return (
     <>
-      <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
-        <div className={styles.centered}>
-          <div className={styles.modal}>
-            <div className={styles.modalHeader}>
-              <h5 className="heading">Guest Reservation</h5>
-            </div>
-            <button className="closeBtn" onClick={() => setIsOpen(false)}>
-              <RiCloseLine style={{ marginBottom: "-3px" }} />
-            </button>
-            <div className="modalContent">
-              Would you like to continue as a Guest or would you like to make an account?
-            </div>
-            <div className="modalActions">
-              <div className="actionsContainer">
-                <button className="GuestBtn" onClick={() => setGoToReservationPage(true)}>
-                  Continue as Guest
-                </button>
-                <button
-                  className="SignInBtn"
-                  onClick={() => setGoToSignIn(true)}>
-                  Sign In
-                </button>
+      <form className='modal-container'>
+        <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+          <div className={styles.centered}>
+            <div className={styles.modal}>
+              <div className={styles.modalHeader}>
+                <h5 className="heading">Guest Reservation</h5>
+              </div>
+              <button className="closeBtn" onClick={() => setIsOpen(false)}>
+                <RiCloseLine style={{ marginBottom: "-3px" }} />
+              </button>
+              <div className="modalContent">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              </div>
+              <div className="modalActions">
+                <div className="actionsContainer">
+                  <button className="GuestBtn" onClick={() => setGoToReservationPage(true)}>
+                    Continue as Guest
+                  </button>
+                  <button
+                    className="SignInBtn"
+                    onClick={() => setGoToSignIn(true)}>
+                    Sign In
+                  </button>
               </div>
             </div>
           </div>
         </div>
+      </form>
     </>
   )
 };
