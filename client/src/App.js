78e1734e92a4a4bed7a7dc375d09bccb9750618d
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2022-12-01 18:08:07 -0600

    Added the confirmation page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 //import statement for bootstrap
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 //Setting up the default login routes
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Landing from "./Pages/landing"
-import Welcome from "./Pages/Welcome"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./Pages/landing";
+import Welcome from "./Pages/Welcome";
 import GuestReserve from "./components/GuestReserve";
-import Login from "./Pages/Login"
-import Register from "./Pages/Register"
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Confirmation from "./Pages/Confirmation_page";
 
 function App() {
   return (
@@ -18,8 +19,9 @@ function App() {
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/GuestReserve" element={<GuestReserve />}/>
-          <Route path="/userReserve" element={<userReserve />}/>
+          <Route path="/GuestReserve" element={<GuestReserve />} />
+          <Route path="/userReserve" element={<userReserve />} />
+          <Route path="/confirmation" element={<Confirmation />} />
         </Routes>
       </Router>
   );
---
+++ client/src/Pages/Confirmation_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+
+function confirmation_page() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default confirmation_page
